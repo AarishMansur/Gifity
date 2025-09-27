2286f61c1d3266c4cba5518e55d86021810b7b52
Aarish Mansur <aarishmansur@gmail.com>
2025-09-27 12:04:28 +0530

    Add COOP/COEP headers for ffmpeg.wasm

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
+  plugins: [react(),tailwindcss()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/core']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ffmpeg: ['@ffmpeg/ffmpeg']
+        }
+      }
+    }
+  }
 })
